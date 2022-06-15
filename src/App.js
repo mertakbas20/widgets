65e759a099ac4506276aaad8879822414f939e2e
Mertakbas <mertakbas@saatteknoloji.com>
2022-06-15 15:09:38 +0300

    text ınputs with hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
     {
         title: 'What is React?',
-        content: 'React is a front end javascript framework'
+        content: 'React is a front end javascript framework',
     },
     {
         title: 'Why use React?',
-        content: 'React is a favorite JS library among engineers'
+        content: 'React is a favorite JS library among engineers',
     },
     {
         title: 'How do you use React?',
-        content: 'You use React by creating components'
+        content: 'You use React by creating components',
     },
 ];
-
 export default () => {
     return (
-        <h1>
-            <Accordion items={items}/>
-        </h1>
+        <div>
+            <Search />
+        </div>
     );
 
 };
