4b62cee65a24bb63bbe31ba7ce67aa0f26a74640
Mertakbas <mertakbas@saatteknoloji.com>
2022-06-16 12:36:23 +0300

    only search with a term

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,8 +19,32 @@ const Search = () => {
 
             setResults(data.query.search);
         };
-        search();
+        if(term) {
+            search();
+        }
     }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className='item'>
+                <div className='right floated content'>
+                    <a 
+                    className='ui button'
+                    href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
+                </div>
+                <div className='content'>
+                    <div className='header'>
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                </div>
+            </div>
+        );
+    });
+
     return (
         <div>
             <div className='ui form'>
@@ -31,6 +55,9 @@ const Search = () => {
                         onChange={e => setTerm(e.target.value)}
                         className='input' />
                 </div>
+            </div>
+            <div className='ui celled list'>
+                {renderedResults}
             </div>
         </div>
     );
